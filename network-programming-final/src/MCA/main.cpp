2a0a135fbda2583d55ccf85eedb92d1364aefa48
shay <scarlat.shay@gmail.com>
2015-06-10 18:57:30 +0300

    Created two main methods for the two components

---
+++ network-programming-final/src/MCA/main.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "I'm a Message Client App" << endl;
+}
---
+++ network-programming-final/src/MSA/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "I'm a Message Server App" << endl;
+	return 0;
+}
