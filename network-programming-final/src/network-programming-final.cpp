1376b15237b414c2a220cc03a997dfd9d467158b
shay <scarlat.shay@gmail.com>
2015-06-10 18:41:47 +0300

    first commit

---
+++ network-programming-final/src/network-programming-final.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : network-programming-final.cpp
+// Author      : Shay & Nethanel
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
